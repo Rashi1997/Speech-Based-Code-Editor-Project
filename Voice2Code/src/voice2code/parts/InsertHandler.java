acf8158d2b3f559b6a87a8349822f0352b6bbeaf
yuanningma <yuanning_ma@brown.edu>
2020-12-01 18:24:25 -0500

    up and next commands working, down functionality needs to be looked into

--- Voice2Code/src/voice2code/parts/InsertHandler.java
+++ Voice2Code/src/voice2code/parts/InsertHandler.java
@@ -31,19 +31,65 @@ public class InsertHandler {
 				try {
 					if("down".equals(roc.tokenize(text)))
 					{
+//						System.out.println("Current count: " + styledText.getCharCount());
+//						System.out.println("Current line: " + styledText.getLineAtOffset(styledText.getCaretOffset()));
 						document.replace(offset, 0, text.replace("down", ";"));
 						styledText.setSelection(styledText.getCharCount());
+						
+//						System.out.println("New count: " + styledText.getCharCount());
 						int currentLine = styledText.getLineAtOffset(styledText.getCaretOffset());
 			            String textAtLine = styledText.getLine(currentLine);
-			            //int spaces = getLeadingSpaces(textAtLine);
+//			            int spaces = getLeadingSpaces(textAtLine);
 			            styledText.insert("\n");
 			            styledText.setCaretOffset(styledText.getCaretOffset());
+//			            for (int i = 0; i < spaces; i++) {
+//			            	styledText.append(" ");
+//			            }
+//			            System.out.println("Final offset: " + styledText.getCaretOffset());
+//			            System.out.println("Final line: " + styledText.getLineAtOffset(styledText.getCaretOffset()));
+			            
+			            styledText.setCaretOffset(offset + text.length() + 1);
+					}
+					else if("up".equals(roc.tokenize(text)))
+					{
+						
+						document.replace(offset, 0, text.replace("up", ""));
+						
+						int currentLine = styledText.getLineAtOffset(styledText.getCaretOffset());
+						System.out.println("Current offset: " + styledText.getCaretOffset());
+						System.out.println("Current line: " + currentLine);
+						int newLoc = styledText.getOffsetAtLine(currentLine - 1);
+//			            String textAtLine = styledText.getLine(currentLine);
+			            //int spaces = getLeadingSpaces(textAtLine);
+//			            styledText.insert("\n");
+//						styledText.setSelection(newLoc);
+			            styledText.setCaretOffset(newLoc);
+			            System.out.println("Final offset: " + newLoc);
+			            System.out.println("Final line: " + styledText.getLineAtOffset(newLoc));
+					} else if ("next".equals(roc.tokenize(text))) {
+						document.replace(offset, 0, text.replace("next", ""));
+						
+						int currentLine = styledText.getLineAtOffset(styledText.getCaretOffset());
+						System.out.println("Current offset: " + styledText.getCaretOffset());
+						System.out.println("Current line: " + currentLine);
+						int newLoc = styledText.getOffsetAtLine(currentLine + 1);
+//			            String textAtLine = styledText.getLine(currentLine);
+			            //int spaces = getLeadingSpaces(textAtLine);
+//			            styledText.insert("\n");
+//						styledText.setSelection(newLoc);
+			            styledText.setCaretOffset(newLoc);
+			            System.out.println("Final offset: " + newLoc);
+			            System.out.println("Final line: " + styledText.getLineAtOffset(newLoc));
+					}
+					else {
+						styledText.setCaretOffset(offset + text.length() + 1);
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				
-				styledText.setCaretOffset(offset + text.length() + 1);
+//				styledText.setCaretOffset(offset + text.length() + 1);
+				
 				
 				//Try while loop
 			}
