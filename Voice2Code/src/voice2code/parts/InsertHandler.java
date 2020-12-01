19ca2cdd4e2cdab995d5a6f57031308a6f5f70d6
DAVID EISELE <eiseleappes@DAVIDs-MacBook-Pro.local>
2020-12-01 18:21:10 -0500

    move forward/back commands

--- Voice2Code/src/voice2code/parts/InsertHandler.java
+++ Voice2Code/src/voice2code/parts/InsertHandler.java
@@ -5,6 +5,7 @@ import org.eclipse.ui.texteditor.*;
 
 import edu.brown.cs.voice2text.ResponseObserverClass;
 
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Control;
@@ -38,18 +39,106 @@ public class InsertHandler {
 			            //int spaces = getLeadingSpaces(textAtLine);
 			            styledText.insert("\n");
 			            styledText.setCaretOffset(styledText.getCaretOffset());
+					} else if("back".equals(roc.tokenize(text)))
+					{
+						System.out.println("orig " + offset);
+						styledText.setSelection(styledText.getCharCount());
+						int currentLine = styledText.getLineAtOffset(styledText.getCaretOffset());
+			            currentLine = Integer.max(0, currentLine - 1);
+			            int lineOffset = getOffsetWithinLine(currentLine, offset, document);
+						String textAtLine = styledText.getLine(currentLine);
+						String textBefore = textAtLine.substring(0, lineOffset);
+						System.out.println(moveBack(textBefore));
+						System.out.println(offset);
+						System.out.println(offset + moveBack(textBefore));
+			            styledText.setCaretOffset(offset + moveBack(textBefore));
+			            
+					} else if("forward".equals(roc.tokenize(text)))
+					{
+						
+						styledText.setSelection(styledText.getCharCount());
+						int currentLine = styledText.getLineAtOffset(styledText.getCaretOffset());
+			            currentLine = Integer.max(0, currentLine - 1);
+			            int lineOffset = getOffsetWithinLine(currentLine, offset, document);
+						String textAtLine = styledText.getLine(currentLine);
+						String textAfter = textAtLine.substring(lineOffset);
+						System.out.println(moveForward(textAfter));
+			            styledText.setCaretOffset(offset + moveForward(textAfter));
 					}
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				
-				styledText.setCaretOffset(offset + text.length() + 1);
+				// commented out
+				//styledText.setCaretOffset(offset + text.length() + 1);
 				
 				//Try while loop
 			}
 			
 		});
 	}
+	
+	private int getOffsetWithinLine(int lineNumber, int offset, IDocument doc) throws BadLocationException {
+		int sum = 0;
+        for (int i = 0 ; i < lineNumber; i++) {
+        	sum += doc.getLineLength(i);
+        }
+        return offset - sum;
+	}
+	
+	// Returns offset of last word
+	private int moveBack(String text) {
+		
+		int index = text.length() - 1;
+		int start = index;
+		// Move back past any letters/nums
+		while (index >= 0) {
+			char c = text.charAt(index);
+			if (Character.isDigit(c) || Character.isLetter(c)) {
+				index--;
+			} else {
+				break;
+			}
+		}
+		// Move back past any non letters/nums
+		while (index >= 0) {
+			char c = text.charAt(index);
+			if (!(Character.isDigit(c) || Character.isLetter(c))) {
+				index--;
+			} else {
+				break;
+			}
+		}
+		return index - start;
+	}
+	
+	    // Returns offset of next word
+		private int moveForward(String text) {
+			
+			int index = 0;
+			int start = index;
+			// Move forward past any letters/nums
+			while (index < text.length()) {
+				char c = text.charAt(index);
+				if (Character.isDigit(c) || Character.isLetter(c)) {
+					index++;
+				} else {
+					break;
+				}
+			}
+			// Move forward past any non letters/nums
+			while (index < text.length()) {
+				char c = text.charAt(index);
+				if (!(Character.isDigit(c) || Character.isLetter(c))) {
+					index++;
+				} else {
+					break;
+				}
+			}
+			return index - start;
+		}
+	
 }
 	
 //	private void moveCursorToLineNumber(int lineNumber) {
@@ -64,4 +153,4 @@ public class InsertHandler {
 //		}
 //		
 //	}
-}
+
